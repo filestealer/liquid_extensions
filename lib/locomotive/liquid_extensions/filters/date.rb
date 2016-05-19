faddeaff9aa91fbf3ec58262fea677415c2cb00c
Gleb Stiblo <errandir@gmail.com>
2016-05-19 18:55:07 +0600

    added prev|next

--- lib/locomotive/liquid_extensions/filters/date.rb
+++ lib/locomotive/liquid_extensions/filters/date.rb
@@ -1,3 +1,6 @@
+require 'date'
+require 'time'
+
 module Locomotive
 	module LiquidExtensions
 		module Filters
@@ -6,14 +9,50 @@ module Locomotive
 					Time.parse(input)
 				end
 
+				def parse_date(input)
+					Date.parse(input)
+				end
+
+				def parse_datetime(input)
+					DateTime.parse(input)
+				end
+
 				def next_date(input, *opts)
-					p input
-					p opts
+					in_date = if input.class == String
+								  DateTime.parse input
+							  else
+								  input.to_datetime
+							  end
+					in_date.send(
+						case opts[1]
+						when 'years'
+							:next_year
+						when 'months'
+							:next_month
+						else
+							:next_day
+						end,
+						opts[0].to_i
+					)
 				end
 
 				def prev_date(input, *opts)
-					p input
-					p opts
+					in_date = if input.class == String
+								  DateTime.parse input
+							  else
+								  input.to_datetime
+							  end
+					in_date.send(
+						case opts[1]
+						when 'years'
+							:prev_year
+						when 'months'
+							:prev_month
+						else
+							:prev_day
+						end,
+						opts[0].to_i
+					)
 				end
 			end
 		end
