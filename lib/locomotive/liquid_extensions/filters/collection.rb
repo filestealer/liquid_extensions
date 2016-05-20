a9c8318f9c2f81b2e038ca01e689a5950135f96a
Gleb Stiblo <errandir@gmail.com>
2016-05-20 10:47:30 +0600

    added slice

--- lib/locomotive/liquid_extensions/filters/collection.rb
+++ lib/locomotive/liquid_extensions/filters/collection.rb
@@ -5,6 +5,10 @@ module Locomotive
 				def chunk_by(input, *opts)
 					input.each.to_a.each_slice(opts[0]).to_a
 				end
+
+				def slice(input, *opts)
+					input.each.to_a.slice(opts[0].to_i, (opts[1] || -1).to_i)
+				end
 			end
 		end
 	end
