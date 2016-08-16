fc929bc1d05246b39633fea230ce46400c370d2e
Gleb Stiblo <errandir@gmail.com>
2016-08-16 16:50:45 +0600

    debug

--- lib/locomotive/liquid_extensions/filters/collection.rb
+++ lib/locomotive/liquid_extensions/filters/collection.rb
@@ -9,6 +9,19 @@ module Locomotive
 				def slice(input, *opts)
 					input.each.to_a.slice(opts[0].to_i, (opts[1] || 1).to_i)
 				end
+
+				def log_it(input, *opts)
+					Rails.logger.fatal "XDBG>>>#{input.inspect}"
+					input
+				end
+
+				def inspect_it(input, *opts)
+					input.inspect.to_s
+				end
+
+				def debug_it(input, *opts)
+					binding.pry
+				end
 			end
 		end
 	end
